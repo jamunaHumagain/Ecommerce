79402f908a10e48033cd151b0627cb542bf35728
jamunaHumagain <109575769+jamunaHumagain@users.noreply.github.com>
2024-11-24 18:16:12 +0545

    Project Completed

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./DescriptionBox.css"
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+<div className="descriptionbox-nav-box">Description</div>
+<div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+<div className="descriptionbox-description">
+    <p>
+    An eCommerce website is an online platform where businesses can sell products or services to customers over the internet. It serves as a digital storefront, allowing users to browse, select, and purchase items securely from anywhere, often with features like product search, filters, reviews, and payment gateways. eCommerce websites enable businesses to reach a global audience, operate 24/7, and provide convenience to customers by eliminating the need for physical shopping. Popular examples include Amazon, eBay, and Shopify-based stores, catering to a wide range of industries and niches.</p>
+<p>
+An eCommerce website offers a wide range of features to ensure a seamless shopping experience for customers and efficient management for businesses. It typically includes a user-friendly interface with intuitive navigation, product categories, filters, and a robust search function to help customers find items easily. Detailed product listings with descriptions, images, pricing, and customer reviews provide all necessary information. 
+
+</p>
+
+
+
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default DescriptionBox
